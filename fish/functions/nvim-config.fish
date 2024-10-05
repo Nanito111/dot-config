c3a9d3d9a574595b2a02c09f357fe36ed50f779e
Nanito111 <fgaldames79@gmail.com>
2024-10-05 15:40:46 -0300

    remake nvim-config alias, change kitty theme to kanagawa-dragon

--- fish/functions/nvim-config.fish
+++ fish/functions/nvim-config.fish
@@ -1,4 +1,4 @@
-function nvim-config --wraps='cd /home/nanito/.config/nvim/lua/custom; nvim' --wraps=cd\ \'/home/nanito/.config/nvim/lua\'\;\ nvim --description alias\ nvim-config=cd\ \'/home/nanito/.config/nvim/lua\'\;\ nvim
-  cd '/home/nanito/.config/nvim/lua'; nvim $argv
+function nvim-config --wraps='cd /home/nanito/.config/nvim/lua/custom; nvim' --wraps=cd\ \'/home/nanito/.config/nvim/lua\'\;\ nvim --wraps='cd ~/.config/nvim/lua/;nvim' --wraps='cd ~/.config/nvim/lua/;nvim .' --description 'alias nvim-config cd ~/.config/nvim/lua/;nvim'
+  cd ~/.config/nvim/lua/;nvim $argv
         
 end
