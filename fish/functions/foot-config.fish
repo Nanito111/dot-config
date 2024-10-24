83459d7aa645abad59fd1f2c170153e0b9f3780a
Nanito111 <fgaldames79@gmail.com>
2024-10-24 04:11:24 -0300

    add cursor color to foot and alias for foot config

---
+++ fish/functions/foot-config.fish
@@ -0,0 +1,4 @@
+function foot-config --wraps='cd ~/.config/foot/ & nvim foot.ini' --description 'alias foot-config cd ~/.config/foot/ & nvim foot.ini'
+  cd ~/.config/foot/ & nvim foot.ini $argv
+        
+end
