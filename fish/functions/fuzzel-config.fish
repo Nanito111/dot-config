c828c0ad000efbd12eb90f969e35811b15db8fdd
Nanito111 <fgaldames79@gmail.com>
2024-12-12 02:39:43 -0300

    add fuzzel and lazygit, mod hyprland config

---
+++ fish/functions/fuzzel-config.fish
@@ -0,0 +1,4 @@
+function fuzzel-config --wraps='cd /home/francisco/.config/fuzzel && nvim fuzzel.ini' --description 'alias fuzzel-config=cd /home/francisco/.config/fuzzel && nvim fuzzel.ini'
+  cd /home/francisco/.config/fuzzel && nvim fuzzel.ini $argv
+        
+end
