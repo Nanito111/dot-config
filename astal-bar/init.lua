78b893d7dbc88148f1e0a1984c2df4ac82f4026a
Nanito111 <fgaldames79@gmail.com>
2024-12-28 02:37:29 -0300

    added realtime css editing astal-bar

--- astal-bar/init.lua
+++ astal-bar/init.lua
@@ -6,8 +6,19 @@ local src = require("lib").src
 
 local scss = src("style.scss")
 local css = "/tmp/style.css"
-
+-- process scss at start
 astal.exec("sass " .. scss .. " " .. css)
+
+local function reload_css(_, event)
+	if event == 0 then
+		print("style changes detected, reloading CSS")
+		astal.exec("sass " .. scss .. " " .. css)
+		print("applying CSS to App")
+		App:apply_css(css)
+	end
+end
+
+astal.monitor_file("style.scss", reload_css)
 
 App:start({
 	css = css,
