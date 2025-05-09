614466d23a9f7af0f02a902d221059905a351a50
Nanito111 <fgaldames79@gmail.com>
2025-05-09 00:21:35 -0400

    delete astal-bar function

--- fish/functions/astal-bar.fish
+++
@@ -1,4 +0,0 @@
-function astal-bar --wraps='lua ~/.config/astal-bar/init.lua' --wraps='cd ~/.config/astal-bar && lua init.lua' --description 'alias astal-bar=cd ~/.config/astal-bar && lua init.lua'
-  cd ~/.config/astal-bar && lua init.lua $argv
-        
-end
