98d182cbb0c632b91f8507bb392a063f0a1f51e4
Nanito111 <fgaldames79@gmail.com>
2025-05-10 20:16:40 -0400

    better system-update notification

--- hypr/checkupdates.fish
+++ hypr/checkupdates.fish
@@ -1,10 +1,19 @@
 #!/bin/fish
 
-set updates_count $(checkupdates | wc -l)
+set -l updates_count $(checkupdates | wc -l)
 
 if test $updates_count -gt 0
+  # send notification and read result of notification
   notify-send "Updates Available" "The are $updates_count pending updates" \
-    -i software-updates-symbolic -t 7000
+    -i software-updates-symbolic -a "System" -A "ok"=update \
+    | read -l update_confirmation
+
+  if test "$update_confirmation" = "ok"
+    echo "Update Confirmed"
+    uwsm app -- foot sudo pacman -Syu
+  else
+    echo "Update Cancelled"
+  end
 end
 
 set -e updates_count
