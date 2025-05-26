7f410468b8e53db4be02eac5d645fe8a8fc61b1b
Nanito111 <fgaldames79@gmail.com>
2025-05-26 12:09:39 -0400

    lazy load conda, starship show base conda env name

--- fish/config.fish
+++ fish/config.fish
@@ -4,18 +4,21 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/francisco/miniforge3/bin/conda
-    eval /home/francisco/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/francisco/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/francisco/miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/francisco/miniforge3/bin" $PATH
-    end
-end
+# Uses the first conda installation found in the following list
+set -x CONDA_PATH $HOME/miniforge/bin/conda /opt/miniforge/bin/conda
 
-if test -f "/home/francisco/miniforge3/etc/fish/conf.d/mamba.fish"
-    source "/home/francisco/miniforge3/etc/fish/conf.d/mamba.fish"
+function conda
+    echo "Lazy loading conda upon first invocation..."
+    functions --erase conda
+    for conda_path in $CONDA_PATH
+        if test -f $conda_path
+            echo "Using Conda installation found in $conda_path"
+            eval $conda_path "shell.fish" "hook" | source
+            conda $argv
+            return
+        end
+    end
+    echo "No conda installation found in $CONDA_PATH"
 end
 # <<< conda initialize <<<
 
