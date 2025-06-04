fac5479b061efa12a50dbab8c0c38d6f6d18c22e
Nanito111 <fgaldames79@gmail.com>
2025-06-04 19:42:19 -0400

    (fish) add description to function: funcdel

--- fish/functions/funcdel.fish
+++ fish/functions/funcdel.fish
@@ -1,4 +1,4 @@
-function funcdel
+function funcdel --description 'Delete function by name'
   if test -e ~/.config/fish/functions/$argv[1].fish
     rm ~/.config/fish/functions/$argv[1].fish
     echo "Deleted function" $argv[1]
