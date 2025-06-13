f89c4bd23e3ef1ff3a173dd9cb18cfdd8dcaf2f6
Nanito111 <fgaldames79@gmail.com>
2025-06-12 21:12:06 -0400

    (sleep-better) fix bash script, old song path

--- bash/scripts/sleep-better/sleep-better.bash
+++ bash/scripts/sleep-better/sleep-better.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 notify-send "Good night..." "Sleep-better process is starting. System will be shutdown." \
-  -i system-suspend -a "Sleep-better"
+    -i system-suspend -a "Sleep-better"
 
-ffplay $HOME/.config/sleep-better/sleep-song.ogg -autoexit -nodisp
+ffplay $HOME/.config/bash/scripts/sleep-better/sleep-song.ogg -autoexit -nodisp
 
 shutdown now
