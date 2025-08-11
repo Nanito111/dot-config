8de416a22dff0c1d349b7eafa5cd3ba755db4cdf
Nanito111 <fgaldames79@gmail.com>
2025-08-11 11:07:28 -0400

    (sleep-better) change sleep outro song

--- bash/scripts/sleep-better/sleep-better.bash
+++ bash/scripts/sleep-better/sleep-better.bash
@@ -3,7 +3,6 @@
 notify-send "Good night..." "Sleep-better process is starting. System will be shutdown." \
     -i system-suspend -a "Sleep-better"
 
-ffplay "$BASH_CONFIG_DIR/scripts/sleep-better/sleep-audio.ogg" -nodisp -autoexit &
-ffplay "$BASH_CONFIG_DIR/scripts/sleep-better/sleep-song.ogg" -nodisp -autoexit
+ffplay "$BASH_CONFIG_DIR/scripts/sleep-better/soft-outro-piano.mp3" -nodisp -autoexit
 
 shutdown now
