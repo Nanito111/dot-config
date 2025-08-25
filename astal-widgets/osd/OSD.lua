728719f47e1d7c24ccf9f12cf9ff30986b3cc5d0
Nanito111 <fgaldames79@gmail.com>
2025-08-25 13:19:30 -0400

    (astal) new astal-osd widget

---
+++ astal-widgets/osd/OSD.lua
@@ -0,0 +1,82 @@
+local astal = require("astal")
+local Widget = require("astal.gtk3").Widget
+local Anchor = astal.require("Astal").WindowAnchor
+local Variable = require("astal").Variable
+local bind = astal.bind
+local timeout = astal.timeout
+
+local Wp = astal.require("AstalWp")
+local speaker = Wp.get_default().audio.default_speaker
+local microphone = Wp.get_default().audio.default_microphone
+
+local SHOW_TIME = 1 * 1000
+local osd_icon = Variable.new()
+local osd_level = Variable.new()
+local audio_is_playing = Variable.new(false)
+
+osd_level:subscribe(function()
+    -- if audio is not playing, play audio
+    if audio_is_playing:get() == false then
+        audio_is_playing:set(true)
+        astal.exec_async("canberra-gtk-play -i audio-volume-change", function()
+            audio_is_playing:set(false)
+        end)
+    end
+end)
+
+local function show(window, window_timeout, level, icon)
+    if window_timeout:get() ~= nil then
+        window_timeout:get():cancel()
+    end
+    window_timeout:set(timeout(SHOW_TIME, function()
+        window:hide()
+    end))
+    osd_icon:set(icon)
+    osd_level:set(level)
+    window:show()
+end
+
+return function(gdkmonitor)
+    local window_timeout = Variable.new()
+
+    return Widget.Window({
+        setup = function(self)
+            local window = self
+            -- speaker
+            self:hook(speaker, "notify::volume", function(_)
+                show(window, window_timeout, speaker.volume, speaker.volume_icon)
+            end)
+            self:hook(speaker, "notify::volume-icon", function(_)
+                show(window, window_timeout, speaker.volume, speaker.volume_icon)
+            end)
+
+            -- microphone
+            self:hook(microphone, "notify::volume", function(_)
+                show(window, window_timeout, microphone.volume, microphone.volume_icon)
+            end)
+            self:hook(microphone, "notify::volume-icon", function(_)
+                show(window, window_timeout, microphone.volume, microphone.volume_icon)
+            end)
+        end,
+        class_name = "OnScreenDisplay",
+        gdkmonitor = gdkmonitor,
+        namespace = "astal-osd",
+        anchor = Anchor.BOTTOM,
+        margin_bottom = 300,
+        width = 180,
+        height = 38,
+        layer = "OVERLAY",
+        visible = false,
+        Widget.Box({
+            class_name = "base",
+            Widget.Icon({
+                icon = bind(osd_icon),
+            }),
+            Widget.LevelBar({
+                valign = "CENTER",
+                hexpand = true,
+                value = bind(osd_level),
+            }),
+        }),
+    })
+end
---
+++ astal-widgets/osd/init.lua
@@ -0,0 +1,51 @@
+local App = require("astal.gtk3.app")
+local astal = require("astal")
+
+local OnScreenDisplay = require("osd.OSD")
+local utils = require("utils")
+local src = utils.src
+local reload_css = utils.reload_css
+local create_css = utils.create_css_from_sass
+
+local main = "main.scss"
+local scss = src("./osd.scss")
+local css = "/tmp/astal-osd.css"
+local INSTANCE_NAME = "osd"
+
+local function on_reload_css(_, event)
+    reload_css(event, App, scss, css)
+end
+
+-- process scss at start
+create_css(scss, css)
+astal.monitor_file(scss, on_reload_css)
+astal.monitor_file(main, on_reload_css)
+
+local reloading_app = false
+-- check changes in widgets
+local function reload_app()
+    if not reloading_app then
+        reloading_app = true
+        astal.exec_async("systemctl --user restart astal@" .. INSTANCE_NAME)
+    end
+end
+
+astal.monitor_file(src("OSD.lua"), reload_app)
+astal.monitor_file(src("init.lua"), reload_app)
+astal.monitor_file("utils.lua", reload_app)
+
+App:start({
+    instance_name = INSTANCE_NAME,
+    css = css,
+    request_handler = function(msg, res)
+        print(msg)
+        res("ok")
+    end,
+    main = function()
+        for _, mon in pairs(App.monitors) do
+            OnScreenDisplay(mon)
+            print(mon.model)
+        end
+        print("astal-" .. INSTANCE_NAME .. " started")
+    end,
+})
