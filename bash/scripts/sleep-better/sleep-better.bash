f2f3a22e8db78d142ef4e8b9ccbdab35e4a7900f
Nanito111 <fgaldames79@gmail.com>
2025-09-13 15:49:12 -0300

    (bash) sleep-better fix sound

--- bash/scripts/sleep-better/sleep-better.bash
+++ bash/scripts/sleep-better/sleep-better.bash
@@ -3,6 +3,6 @@
 notify-send "Good night..." "Sleep-better process is starting. System will be shutdown." \
     -i system-suspend -a "Sleep-better" -t 2000
 
-canberra-gtk-play -i _desktop-logout
+canberra-gtk-play -i desktop-logout
 
 shutdown now
