10a89acbd6a3f19dce4cdcdb5f6f26be088bec7c
Nanito111 <fgaldames79@gmail.com>
2025-10-23 13:25:27 -0300

    bash: remove conda.bash, move mamba and conda source to config.bash

--- bash/config.bash
+++ bash/config.bash
@@ -12,3 +12,18 @@ for file in $BASH_CONFIG_DIR/functions/*.bash; do
 done
 
 [ -f "$BASH_CONFIG_DIR/aliases.bash" ] && source "$BASH_CONFIG_DIR/aliases.bash"
+
+[ -f /opt/miniforge/etc/profile.d/conda.sh ] && source /opt/miniforge/etc/profile.d/conda.sh
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+export MAMBA_EXE='/opt/miniforge/bin/mamba';
+export MAMBA_ROOT_PREFIX='/home/francisco/.conda';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
