4a7e74f60ef5d2b95333c59cf5b22b6eaf1d9f29
Nanito111 <fgaldames79@gmail.com>
2025-10-25 00:12:27 -0300

    nvim: add syntax highlighting to folds

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -42,37 +42,10 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.o.cursorlineopt = "both"
 vim.cmd.set "guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,r-cr:hor20,o:hor50"
 
--- vim.api.nvim_create_autocmd("ModeChanged", {
---   callback = function()
---     if vim.fn.mode() == "n" then
---       vim.api.nvim_get_hl_by_name "hola"
---     end
---   end,
--- })
-
--- Fold Stuff
+-- Folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-function _G.my_foldtext()
-  -- inicio y fin del pliegue
-  local start = vim.v.foldstart
-  local finish = vim.v.foldend
-  local count = finish - start + 1
-
-  -- obtener la primera línea del pliegue
-  local line = vim.api.nvim_buf_get_lines(0, start - 1, start, false)[1] or ""
-  line = vim.trim(line)
-
-  -- limitar longitud y añadir indicador de pliegue
-  if #line > 80 then
-    line = line:sub(1, 77) .. "..."
-  end
-
-  -- construir texto final
-  return string.format("%s  [%d lines]", line, count)
-end
-
-vim.opt.foldtext = "v:lua.my_foldtext()"
-vim.opt.fillchars = { fold = " ", foldopen = "▼", foldclose = "▶", foldsep = "│" }
+vim.opt.foldtext = ""
+vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "▼", foldclose = "▶", foldsep = "│" }
 vim.wo.foldcolumn = "auto:5"
+vim.opt.foldlevelstart = 99
