9b2f14318c403a70489cdf3198f9e2d69a6e2558
Nanito111 <fgaldames79@gmail.com>
2025-11-12 17:12:30 -0300

    nvim: autocmd to use lsp folding expr when available

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -15,3 +15,15 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     vim.opt.foldlevel = 99
   end,
 })
+
+-- use LSP folding if client supports it
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client:supports_method "textDocument/foldingRange" then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+    end
+  end,
+})
