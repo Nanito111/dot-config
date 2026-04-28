d8b8db4e8bea78cc17bc8f16a307cff65fd1cc9e
Nanito111 <fgaldames79@gmail.com>
2026-04-27 21:51:41 -0400

    general: a lot of changes and caelestia shell integration

--- bash/functions/config.bash
+++ bash/functions/config.bash
@@ -1,14 +1,13 @@
 declare -A options
 options[hypr]="Hyprland configuration"
 options[bash]="Bash configuration"
-options[astal]="Astal widgets configuration"
 options[nvim]="Neovim configuration"
 options[fuzzel]="Fuzzel launcher configuration"
 options[foot]="Foot terminal configuration"
+options[caelestia]="Caelestia shell configuration"
 options[uwsm]="UWSM environment configuration"
 options[starship]="Starship prompt configuration"
 options[fastfetch]="Fastfetch configuration"
-options[matugen]="Matugen theme configuration"
 options[sunsetr]="Sunsetr display configuration"
 options[systemd]="Systemd user units configuration"
 options[help]="List of available options"
@@ -17,16 +16,15 @@ config() {
     case "$1" in
         hypr) cd "$XDG_CONFIG_HOME/hypr" && nvim ;;
         bash) cd "$XDG_CONFIG_HOME/bash" && nvim ;;
-        astal) cd "$XDG_CONFIG_HOME/astal-widgets" && nvim ;;
         nvim) cd "$XDG_CONFIG_HOME/nvim" && nvim ;;
         fuzzel) cd "$XDG_CONFIG_HOME/fuzzel" && nvim fuzzel.ini ;;
         foot) cd "$XDG_CONFIG_HOME/foot" && nvim foot.ini ;;
         uwsm) cd "$XDG_CONFIG_HOME/uwsm" && nvim env ;;
         starship) cd "$XDG_CONFIG_HOME" && nvim starship.toml ;;
         fastfetch) cd "$XDG_CONFIG_HOME/fastfetch" && nvim config.jsonc ;;
-        matugen) cd "$XDG_CONFIG_HOME/matugen" && nvim config.toml ;;
         sunsetr) cd "$XDG_CONFIG_HOME/sunsetr" && nvim sunsetr.toml ;;
         systemd) cd "$XDG_CONFIG_HOME/systemd/user" && nvim ;;
+        caelestia) cd "$XDG_CONFIG_HOME/caelestia" && nvim shell.json;;
         help)
 
             echo -e "Config is a utility function that provides quick access to configuration files.\n"
